890d8409f2ed42d6d6acf5646d6618b142250b44
Divyansh Kumar <divyansh0138.becse24@chitkara.edu.in>
2026-05-20 11:23:14 +0530

    Fix user model import case

--- RoofScout/Backend/routes/admin.js
+++ RoofScout/Backend/routes/admin.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const User = require("../models/User");
+const User = require("../models/user");
 const Property = require("../models/Property");
 const { authMiddleware, adminMiddleware } = require("../middleware/authMiddleware");
 
